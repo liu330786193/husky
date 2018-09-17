697f61515bd9ea4bc7b8e1558351c730bece04b2
liu330786193 <330786193@qq.com>
2018-09-18 00:15:12 +0800

    - 继续完善huksy

--- husky-core/src/main/java/com/lyl/husky/core/internal/election/LeaderService.java
+++ husky-core/src/main/java/com/lyl/husky/core/internal/election/LeaderService.java
@@ -1,9 +1,13 @@
 package com.lyl.husky.core.internal.election;
 
+import com.lyl.husky.core.internal.schedule.JobRegistry;
 import com.lyl.husky.core.internal.server.ServerService;
 import com.lyl.husky.core.internal.storage.JobNodeStorage;
+import com.lyl.husky.core.internal.storage.LeaderExecutionCallback;
 import com.lyl.husky.core.reg.base.CoordinatorRegistryCenter;
+import com.lyl.husky.core.util.concurrent.BlockUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.server.quorum.Leader;
 
 /**
  * 主节点服务
@@ -26,7 +30,57 @@ public class LeaderService {
      */
     public void electLeader(){
         log.debug("Elect a new leader now.");
-        jobNodeStorage.executeInLeader();
+        jobNodeStorage.executeInLeader(LeaderNode.LATCH, new LeaderElectionExecutionCallback());
+    }
+
+    /**
+     * 判断您当前节点是否为主节点
+     * <p>
+     *     如果主节点正在选举中而导致取不到主节点，则阻塞至主节点选举完成再返回
+     * </p>
+     */
+    public boolean isLeaderUntilBlock(){
+        while (!hasLeader() && serverService.hasAvailableServers()){
+            log.info("Leader is electing, waiting for {} ms", 100);
+            BlockUtils.waitingShortTime();
+            if (!JobRegistry.getInstance().isShutdown(jobName) && serverService.isAvailableServer(JobRegistry.getInstance().getJobInstance(jobName).getIp())){
+                electLeader();
+            }
+        }
+        return isLeader();
+    }
+
+    /**
+     * 判断当前节点是否为主节点
+     */
+    public boolean isLeader(){
+        return !JobRegistry.getInstance().isShutdown(jobName) && JobRegistry.getInstance()
+                .getJobInstance(jobName)
+                .getJobInstanceId()
+                .equals(jobNodeStorage.getJobNodeData(LeaderNode.INSTANCE));
+    }
+
+    /**
+     * 删除主节点重新选举
+     */
+    public void removeLeader(){
+        jobNodeStorage.removeJobNodeIfExisted(LeaderNode.INSTANCE);
+    }
+
+    /**
+     * 判断是否已经有主节点
+     */
+    public boolean hasLeader(){
+        return jobNodeStorage.isJobNodeExisted(LeaderNode.INSTANCE);
+    }
+
+    public class LeaderElectionExecutionCallback implements LeaderExecutionCallback {
+        @Override
+        public void execute() {
+            if (!hasLeader()){
+                jobNodeStorage.fillEphemeralJobNode(LeaderNode.INSTANCE, JobRegistry.getInstance().getJobInstance(jobName).getJobInstanceId());
+            }
+        }
     }
 
 }
