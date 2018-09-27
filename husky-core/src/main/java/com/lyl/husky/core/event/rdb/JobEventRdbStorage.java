35f121662b8fdd5326c89361523942886b63dd16
liu330786193 <liu19910406>
2018-09-27 18:40:03 +0800

    - 继续完善huksy

--- husky-core/src/main/java/com/lyl/husky/core/event/rdb/JobEventRdbStorage.java
+++ husky-core/src/main/java/com/lyl/husky/core/event/rdb/JobEventRdbStorage.java
@@ -1,9 +1,7 @@
 package com.lyl.husky.core.event.rdb;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @Author lyl
@@ -32,11 +30,65 @@ public final class JobEventRdbStorage {
         }
     }
 
-    private void createJobExecutionTableAndIndexIfNeeded(Connection conn) {
+    private void createJobExecutionTableAndIndexIfNeeded(final Connection conn) throws SQLException {
         DatabaseMetaData dbMetaData = conn.getMetaData();
+        try (ResultSet resultSet = dbMetaData.getTables(null, null, TABLE_JOB_EXECUTION_LOG, new String[]{"TABLE"})){
+            if (!resultSet.next()){
+                createJobExecutionTable(conn);
+            }
+        }
     }
 
-    private void createJobStatusTraceTableAndIndexIfNeeded(Connection conn) {
+    private void createJobStatusTraceTableAndIndexIfNeeded(final Connection conn) throws SQLException {
+        DatabaseMetaData dbMetaData = conn.getMetaData();
+        try (ResultSet resultSet = dbMetaData.getTables(null, null, TABLE_JOB_STATUS_TRACE_LOG, new String[]{"TABLE"})) {
+            if (!resultSet.next()) {
+                createJobStatusTraceTable(conn);
+            }
+        }
+        createTaskIdIndexIfNeeded(conn, TABLE_JOB_STATUS_TRACE_LOG, TASK_ID_STATE_INDEX);
+    }
+
+    private void createTaskIdIndexIfNeeded(Connection conn, String tableJobStatusTraceLog, String taskIdStateIndex) {
+    }
+
+    private void createJobExecutionTable(Connection conn) throws SQLException {
+        String dbSchema = "CREATE TABLE `" + TABLE_JOB_EXECUTION_LOG + "` ("
+                + "`id` VARCHAR(40) NOT NULL, "
+                + "`job_name` VARCHAR(100) NOT NULL, "
+                + "`task_id` VARCHAR(255) NOT NULL, "
+                + "`hostname` VARCHAR(255) NOT NULL, "
+                + "`ip` VARCHAR(50) NOT NULL, "
+                + "`sharding_item` INT NOT NULL, "
+                + "`execution_source` VARCHAR(20) NOT NULL, "
+                + "`failure_cause` VARCHAR(4000) NULL, "
+                + "`is_success` INT NOT NULL, "
+                + "`start_time` TIMESTAMP NULL, "
+                + "`complete_time` TIMESTAMP NULL, "
+                + "PRIMARY KEY (`id`));";
+        try (PreparedStatement preparedStatement = conn.prepareStatement(dbSchema)){
+            preparedStatement.execute();
+        }
+
+    }
+
+    private void createJobStatusTraceTable(final Connection conn) throws SQLException {
+        String dbSchema = "CREATE TABLE `" + TABLE_JOB_STATUS_TRACE_LOG + "` ("
+                + "`id` VARCHAR(40) NOT NULL, "
+                + "`job_name` VARCHAR(100) NOT NULL, "
+                + "`original_task_id` VARCHAR(255) NOT NULL, "
+                + "`task_id` VARCHAR(255) NOT NULL, "
+                + "`slave_id` VARCHAR(50) NOT NULL, "
+                + "`source` VARCHAR(50) NOT NULL, "
+                + "`execution_type` VARCHAR(20) NOT NULL, "
+                + "`sharding_item` VARCHAR(100) NOT NULL, "
+                + "`state` VARCHAR(20) NOT NULL, "
+                + "`message` VARCHAR(4000) NULL, "
+                + "`creation_time` TIMESTAMP NULL, "
+                + "PRIMARY KEY (`id`));";
+        try (PreparedStatement preparedStatement = conn.prepareStatement(dbSchema)) {
+            preparedStatement.execute();
+        }
     }
 
 
